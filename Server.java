6473a2e9a0a0a57c7209aec0aa5058fec3ab2f03
Mattias <mattias9922@hotmail.com>
2022-11-04 12:55:02 +0100

    Update server

--- Server.java
+++ Server.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.awt.*;
 
 
-public class Server {
+public class Server extends Thread {
     private int userId = 0;
 
     public void run() {
@@ -11,39 +11,65 @@ public class Server {
             int serverPort = 2000;
             ServerSocket serverSocket = new ServerSocket(serverPort);
             // serverSocket.setSoTimeout(10000); 
-            while(true){
+            while(true)
+            {
                 System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "..."); 
     
                 // Accept connection from client
                 Socket server = serverSocket.accept();
-                System.out.println("Just connected to " + server.getRemoteSocketAddress()); 
-    
-                // Send data to client
-                PrintWriter toClient = new PrintWriter(server.getOutputStream(),true);
-                // Receive data from client
+                System.out.println("Just connected to " + server.getRemoteSocketAddress());
+
+                // Create a new thread for the client
+                ClientHandler client = new ClientHandler(server, userId);
                 
-                BufferedReader fromClient = new BufferedReader( new InputStreamReader(server.getInputStream()));
-                userId++;
-                
-                // while(true){
-                    String line = fromClient.readLine();
-                    if(line != null)
-                    {
-                        System.out.println("User "+userId+" sent: " + line); 
-                    } 
-                // }
-                    // toClient.println("Thank you for connecting to " + server.getLocalSocketAddress() + "\nGoodbye!"); 
+                synchronized(this) {
+                    userId++;
+                }
+                client.start();
+                client.run();
             }
         }
-        catch(UnknownHostException ex) {
+        catch(UnknownHostException ex) 
+        {
             ex.printStackTrace();
         }
-        catch(IOException e){
+        catch(IOException e)
+        {
             e.printStackTrace();
         }
       }
+
+      static class ClientHandler extends Thread {
+        private volatile Socket clientSocket;
+        private volatile PrintWriter out;
+        private volatile BufferedReader in;
+        private volatile int userId;
+
+        public ClientHandler(Socket socket, int userId) {
+            this.clientSocket = socket;
+            this.userId = userId;
+        }
+
+        public void run() {
+            try {
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) 
+                {
+                    System.out.println("User "+userId+" sent: " + inputLine);
+                    break;
+                }
+            } catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         Server srv = new Server();
+
 		srv.run();
     }
 }
